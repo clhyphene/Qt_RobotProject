83769803a66b49e8a8747a432ecc87c235b9b357
Connor Leckie-Ewing <leckie-ewing.2@osu.edu>
2018-02-14 21:45:54 -0500

    Removed Hello World and included necessary directories

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
 
 int main(void)
 {
 
-    float x,y;
 
-    LCD.Clear( FEHLCD::Black );
-    LCD.SetFontColor( FEHLCD::White );
-
-    while( true )
-    {
-        if( LCD.Touch(&x,&y) )
-        {
-            LCD.WriteLine( "Hello World!" );
-            Sleep( 100 );
-        }
-    }
     return 0;
 }
