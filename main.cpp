4e442e6150cb5505bd9bb32f4f2064a9602c1695
Roope Ketola <ketola.2@osu.edu>
2018-02-23 12:18:39 -0500

    Performance test one completed flawlessly.

--- main.cpp
+++ main.cpp
@@ -29,6 +29,8 @@ void getWorldState(int *rEncVal, int *lEncVal, double *CdSVal);
 void resetEncoders();
 void move_forward(float distance); //using encoders
 void turn(int degrees);
+void dance_turn(int degrees);
+void dance_party();
 
 //Global Variables
 //Input standard motor power levels here
@@ -63,7 +65,23 @@ int main(void)
         Sleep(1.5);
 
         //move forward 2
-        move_forward(9.15);
+        move_forward(8.275);
+        Sleep(1.5);
+
+        //turn 90 degrees counterclockwise
+        turn(-90);
+        Sleep(1.5);
+
+        //move forward 2.1
+        move_forward(6);
+        Sleep(1.5);
+
+        //turn 90 degrees clockwise
+        turn(90);
+        Sleep(1.5);
+
+        //move forward 2.2
+        move_forward(1.25);
         Sleep(1.5);
 
         //turn 90 degrees counterclockwise
@@ -71,7 +89,7 @@ int main(void)
         Sleep(1.5);
 
         //move forward 3
-        move_forward(12.6);
+        move_forward(4.25);
         Sleep(1.5);
 
         //turn 90 degrees counterclockwise
@@ -79,11 +97,11 @@ int main(void)
         Sleep(1.5);
 
         //move forward 4
-        move_forward(3.2);
+        move_forward(3.5);
         Sleep(1.5);
 
         //move backward 5
-        move_forward(-1.4);
+        move_forward(-1.75);
         Sleep(1.5);
 
         //turn 90 degrees counterclockwise
@@ -91,7 +109,7 @@ int main(void)
         Sleep(1.5);
 
         //move forward 6
-        move_forward(12.8);
+        move_forward(12.85);
         Sleep(1.5);
 
         //turn 90 degrees clockwise
@@ -107,7 +125,7 @@ int main(void)
         Sleep(1.5);
 
         //move forward 8
-        move_forward(3.75);
+        move_forward(4.75);
         Sleep(1.5);
 
         //move backward 9
@@ -128,34 +146,61 @@ int main(void)
 
         //move backward 11
         motor_percent = 60;
-        move_forward(-22);
+        move_forward(-24);
         Sleep(1.5);
 
         //All done
-        LCD.Clear(GOLDENROD);
-        LCD.SetFontColor(BLACK);
-        LCD.WriteLine("All done !");
-        LCD.WriteLine(" ");
-        LCD.WriteLine("Dance partyyyy");
-        //dance
-        motor_percent_turn = 60;
-        turn(360);
-        Sleep(.5);
-        motor_percent_turn = 28;
-        turn(30);
-        turn(-30);
-        turn(30);
-        turn(-30);
-        turn(30);
-        turn(-30);
-        turn(30);
-        turn(-30);
+        dance_party();
         Sleep(10.0);
 
     return 0;
 }
 
 //Functions
+void dance_party() {
+    //party time
+    LCD.Clear(GOLDENROD);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("All done !");
+    LCD.WriteLine(" ");
+    LCD.WriteLine("Dance partyyyy");
+    //dance
+    motor_percent_turn = 60;
+    turn(360);
+    Sleep(.5);
+    motor_percent_turn = 28;
+    dance_turn(30);
+    LCD.Clear(GOLDENROD);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(-30);
+    LCD.Clear(CYAN);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(30);
+    LCD.Clear(RED);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(-30);
+    LCD.Clear(GREEN);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(30);
+    LCD.Clear(PINK);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(-30);
+    LCD.Clear(GOLDENROD);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(30);
+    LCD.Clear(WHITE);
+    LCD.SetFontColor(BLACK);
+    LCD.WriteLine("Dance partyyyy");
+    dance_turn(-30);
+    LCD.WriteLine("All done !");
+}
+
 void driveStraight(int percent) {
 
     //************WE COULD MAKE THIS DRIVE UNTIL IT HITS SOMETHING**********
@@ -260,7 +305,46 @@ void turn(int degrees) //using encoders
     }
 
     //determine the number of counts
-    counts = 3.53*sqrt(degrees*degrees);
+    counts = 3.489*sqrt(degrees*degrees);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((leftEnc.Counts() + rightEnc.Counts()) / 2. < counts);
+
+    //Turn off motors
+    rightMotor.Stop();
+    leftMotor.Stop();
+}
+
+void dance_turn(int degrees) //using encoders
+{
+    //make sure motor percents are at correct values
+    left_motor_percent_turn = motor_percent_turn;
+    right_motor_percent_turn = -motor_percent_turn;
+
+    //Reset encoder counts
+    resetEncoders();
+
+
+    //detect whether turning clockwise or counterclockwise
+    if (degrees >= 0) //clockwise
+    {
+    //Set both motors to desired percent
+    rightMotor.SetPercent(right_motor_percent_turn);
+    leftMotor.SetPercent((-1)*(left_motor_percent_turn));
+
+    }
+
+    else //counterclockwise
+    {
+        //Set both motors to desired percent
+        rightMotor.SetPercent((-1)*(right_motor_percent_turn));
+        leftMotor.SetPercent(left_motor_percent_turn);
+
+    }
+
+    //determine the number of counts
+    counts = 3.435*sqrt(degrees*degrees);
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
